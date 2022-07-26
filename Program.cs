44ddce5ce861c037b43a0c9c39fd49960d679126
Firingo <petr.havlin.0605@gmail.com>
2022-07-26 20:52:39 +0200

    Lekce 3 - Proměnné, typový systém a parsování v C# .NET Program na násobení

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Zadejte číslo k zdvojnásobení!");
+
+string s = Console.ReadLine();
+int a = int.Parse(s);
+a = a * 2;
+Console.WriteLine(a);
+Console.ReadKey();
